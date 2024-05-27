c37d05b1e0df61477ffda19ee51a81460a60256b
Adrian Grøn <adriangron7@gmail.com>
2024-05-27 14:19:24 +0200

    Add project files.

---
+++ switch oppgave/data.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_oppgave
+{
+    internal class data
+    {
+        public void Run()
+        {
+            Random random = new Random();
+
+            int dayNumber = random.Next(1, 8);
+
+            Console.WriteLine("Today is ");
+
+            switch (dayNumber)
+            {
+                case 1:
+                   Console.WriteLine("Mandag :(((");
+                break;
+            }
+            switch (dayNumber)
+            {
+                case 2:
+                    Console.WriteLine("Tirsdag");
+                    break;
+            }
+            switch (dayNumber)
+            {
+                case 3:
+                    Console.WriteLine("Onsdag");
+                    break;
+            }
+            switch (dayNumber)
+            {
+                case 4:
+                    Console.WriteLine("Torsdag");
+                    break;
+            }
+            switch (dayNumber)
+            {
+                case 5:
+                    Console.WriteLine("FREDAG!!!");
+                    break;
+            }
+            switch (dayNumber)
+            {
+                case 6:
+                    Console.WriteLine("Lørdag c:");
+                    break;
+            }
+            switch (dayNumber)
+            {
+                case 7:
+                    Console.WriteLine("Søndag :c");
+                    break;
+            }
+        }
+    }
+}
